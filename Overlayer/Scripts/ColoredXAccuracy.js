a8d63d4e4b9bb76b5a7cf8dde65dfd72a0b9e500
tygovanderlinde <112824357+tygovanderlinde@users.noreply.github.com>
2023-11-17 11:15:44 +0100

    changed font, added top-left, changed position and font size of most things

--- Overlayer/Scripts/ColoredXAccuracy.js
+++ Overlayer/Scripts/ColoredXAccuracy.js
@@ -1,6 +1,6 @@
 function CXAccuracy() {
-    if (XAccuracy(10) <= 90) return ('<color=#FF0000>' + XAccuracy(10) + '%' + '</color>')
-    if (XAccuracy(10) > 90 && XAccuracy(10) < 99) return ('<color=#FFD580>' + XAccuracy(10) + '%' + '</color>')
-    if (XAccuracy(10) >= 99) return ('<color=#00FF00>' + XAccuracy(10) + '%' + '</color>')
-  }
+  if (XAccuracy(10) <= 90) return ('<color=#FF0000>' + XAccuracy(10) + '%' + '</color>')
+  if (XAccuracy(10) > 90 && XAccuracy(10) < 99) return ('<color=#FFD580>' + XAccuracy(10) + '%' + '</color>')
+  if (XAccuracy(10) >= 99) return ('<color=#00FF00>' + XAccuracy(10) + '%' + '</color>')
+}
 RegisterTag("CXAccuracy", CXAccuracy, false);
